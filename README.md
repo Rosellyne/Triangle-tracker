c36c4f904cc1692f5ff53722506f5ffcd084de03
Rosellyne <elijahroselyne@gmail.com>
2019-04-27 18:04:48 +0300

    add the action and method 'post' to index.html file

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Triangle-tracker
+Project Title (Portfolio)
+Simple overview of the IP (A static site describing my work as a developer)
+
+Authors
+Rose
+Victor
+Jane
+Project Description
+Paragraph describing the IP
+
+Installations
+Requirements
+VS code/atom
+Chrome browser
+Technologies
+(HTML)
+(CSS)
+(Javascript)
+(Bootstrap)
+BDD
+Categorised my portfolio four main parts, that is, Home, About, Projects, Contacts
+Created the layout for all my categories.
+Finalised my design in a flow chart on how the whole site is supposed to appear.
+Implemented my design
+project setup instructions
+Clone project or download zipped project files.
+Go to the download folder or the destination of the downloaded zipped file, extract the zip file and open project file.
+(Open HTML file in your code editor (atom/vs code). Make your own changes and open the HTML file in the chrome browser to see changes made)
+Link to deployed site:
+Click HERE
+
+License
+Copyright (c) Rosellyne 2019 This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function result(){
+    var firstDimension =parseInt(document.getElementById)
+    var secondDimension = parseInt (document.getElementById)
+    var thirdDimension = parseInt(document.getElementById)
+    var answer = document.getElementById ('answer')
+    if(firstDimension+secondDimension>thirdDimension&&secondDimension+thirdDimension>firstDimension&&firstDimension+thirdDimension>secondDimension){
+        if(firstDimension===secondDimension&& secondDimension===thirdDimension&& firstDimension===thirdDimension){
+         answer ="Equilateral:All sides are equal!";
+         }
+        else if(firstDimension===secondDimension||secondDimension===thirdDimension||firstDimension===thirdDimension){
+            answer ="Isosceles:Two sides are equal!";
+        }
+        else if(firstDimension!==secondDimension||secondDimension!==thirdDimension||firstDimension!==thirdDimension){
+            answer ="Scalene:No sides are equal!";
+        }
+    }
+    else{
+        answer ="Not a triangle";
+    }
+function reset)(){
+    var firstDimension =parseInt(document.getElementById)
+    var secondDimension = parseInt (document.getElementById)
+    var thirdDimension = parseInt(document.getElementById)
+    var answer = document.getElementById ('answer');
+}
+    
+
+    
